0d5c693149c83aa09e12e01668f326d4f372d667
Bammidi Naveen Krishna <bnaveen1794@gmail.com>
2023-11-05 20:03:53 +0530

    Launching Google and searching for the playwright documentation

--- Demo_Python_project/Tests/OpeningGoogleHomepage.py
+++ Demo_Python_project/Tests/OpeningGoogleHomepage.py
@@ -5,3 +5,12 @@ def test_has_title(page: Page):
     page.goto("https://www.google.com/")
     page.wait_for_timeout(2000)
 
+    input_search_text =  "combobox"
+    
+    page.get_by_role(input_search_text).fill("playwright documentation")
+    page.get_by_role(input_search_text).press('Enter')
+    page.wait_for_timeout(2000)
+
+    
+   
+
