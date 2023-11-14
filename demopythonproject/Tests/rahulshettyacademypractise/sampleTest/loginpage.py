380fb8921340197d2e2e7bdf0beb4ef82257a431
Bammidi Naveen Krishna <bnaveen1794@gmail.com>
2023-11-14 23:29:05 +0530

    minor commit

--- demopythonproject/Tests/rahulshettyacademypractise/sampleTest/loginpage.py
+++ demopythonproject/Tests/rahulshettyacademypractise/sampleTest/loginpage.py
@@ -1,23 +1,25 @@
 import re
 from playwright.sync_api import Page, expect
 
-def test_Login_page(page:Page):
+class loginTest:
 
-    loginButton='Login'
-    emailTextBox = "input#email"
-    passwordTextBox = "input#password"
-    checkBoxLocator ="input[type='checkbox']"
-    AuthunticationCheckBox ="input[type='checkbox']"
+    def test_Login_page(page: Page):
 
-     #testconfig
-    username ="automationpractice49@gmail.com"
-    password = "Naveenb1794"
+        loginButton = 'Login'
+        emailTextBox = "input#email"
+        passwordTextBox = "input#password"
+        checkBoxLocator = "input[type='checkbox']"
+        AuthunticationCheckBox = "input[type='checkbox']"
 
-    page.goto("https://rahulshettyacademy.com/")
-    #page.goto('https://sso.teachable.com/secure/9521/identity/login/password')
-    page.get_by_text(loginButton).click()
-    page.locator(AuthunticationCheckBox).click()
-    page.get_by_test_id(emailTextBox).fill(username)
-    page.get_by_test_id(passwordTextBox).fill(password)
-    page.locator(checkBoxLocator).check()
-    expect(checkBoxLocator).ischecked()
+     # testconfig
+        username = "automationpractice49@gmail.com"
+        password = "Naveenb1794"
+
+        page.goto("https://rahulshettyacademy.com/")
+    # page.goto('https://sso.teachable.com/secure/9521/identity/login/password')
+        page.get_by_text(loginButton).click()
+        page.locator(AuthunticationCheckBox).click()
+        page.get_by_test_id(emailTextBox).fill(username)
+        page.get_by_test_id(passwordTextBox).fill(password)
+        page.locator(checkBoxLocator).check()
+        expect(checkBoxLocator).ischecked()
